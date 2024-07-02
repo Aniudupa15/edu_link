dc1cec4ee34002712ca5bec63e8f8cb8b1d6fddb
navaneeth arya <navaneetharyarao@gmail.com>
2024-07-02 18:14:38 +0530

    google authentication

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:edu_link/auth/auth.dart'; // Ensure you import Authpage
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:edu_link/firebase_options.dart';
-//hello
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
